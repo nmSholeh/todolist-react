c055737be3fc1eaf00aad00b87622aa5891f2ef7
nmSholeh <43803402+nmSholeh@users.noreply.github.com>
2021-01-13 23:07:52 +0700

    emotion css todo component

---
+++ src/component/todo/todo.styles.js
@@ -0,0 +1,26 @@
+import { css } from "@emotion/react";
+
+export const todo = ({ theme }) =>
+  css`
+    position: relative;
+
+    flex: 1 0 auto;
+    text-align: center;
+
+    max-height: 34px;
+    margin: 16px 0 0;
+    font-family: "Homemade Apple", sans-serif;
+    font-size: 24px;
+    border-bottom: 1px solid ${theme.color.primary.black};
+  `;
+
+export const todoText = ({ theme, isCompleted }) =>
+  css`
+    width: 100%;
+    position: absolute;
+    left: 50%;
+    transform: translate(-50%);
+    text-decoration: ${isCompleted ? "line-through" : "initial"};
+    color: ${theme.color.primary.black};
+    height: 50px;
+  `;
