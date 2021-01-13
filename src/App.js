a2551e8567b0620c6e6304feb4f63a1dc176ce59
nmSholeh <43803402+nmSholeh@users.noreply.github.com>
2021-01-13 20:59:27 +0700

    emotion theming pada button dan header

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
-import React, { Component } from "react";
+import React from "react";
 import "./styles.css";
+import { ThemeProvider } from "@emotion/react";
 
 import TodoList from "./Pages/TodoList";
 
+const theme = {
+  color: {
+    primary: {
+      black: "#484848",
+      red: "#e06262"
+    }
+  }
+};
+
 export default function App() {
-  return <TodoList />;
+  return (
+    <ThemeProvider theme={theme}>
+      <TodoList />
+    </ThemeProvider>
+  );
 }
--- src/component/header/Header.jsx
+++ src/component/header/Header.jsx
@@ -2,6 +2,7 @@
 // import React from "react";
 import { jsx } from "@emotion/react";
 import PropTypes from "prop-types";
+import { useTheme } from "@emotion/react";
 
 import Button from "../button/Button";
 
@@ -9,10 +10,12 @@ import Button from "../button/Button";
 import * as styles from "./header.styles";
 
 const Header = ({ showAddToggle, showAdd, clearTodos }) => {
+  const theme = useTheme();
+
   return (
     <section css={styles.header}>
       <Button text={showAdd ? "finish" : "Add"} onClick={showAddToggle} />
-      <h1 css={styles.headerTitle}>Todo Lists</h1>
+      <h1 css={styles.headerTitle(theme)}>Todo Lists</h1>
       <Button text="Clear" onClick={clearTodos} color="red" align="right" />
     </section>
   );
